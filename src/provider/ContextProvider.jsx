dc7acb37004af6439ce3a44fd6a3d5ebe674a1b3
YeasinWebDev <ar9485727@gmail.com>
2024-04-25 21:23:30 +0600

    context provider added

---
+++ src/provider/ContextProvider.jsx
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { auth } from '../firebaseCofig';
+
+export const AuthContext = createContext(null)
+function ContextProvider({ children }) {
+
+    const [user, setUser] = useState(null)
+    const [loading, setloading] = useState(true)
+    const logInByGoogle = () => {
+        setloading(true)
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+    }
+
+    const logInByGithub = () => {
+        setloading(true)
+        const provider = new GithubAuthProvider();
+        return signInWithPopup(auth, provider)
+    }
+
+    const createUser = (email, pass) => {
+        // setloading(true);
+        return  createUserWithEmailAndPassword(auth, email, pass);
+    };
+
+    const signIn = (email, pass) => {
+        setloading(true)
+        return signInWithEmailAndPassword(auth, email, pass)
+    }
+
+    const LogOut = () => {
+        setloading(true)
+        signOut(auth)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                setUser(null);
+            }
+            setloading(false);
+        });
+        return () => {
+            unSubscribe()
+        }
+    }, [])
+
+
+
+    const authinfo = { user, setUser, createUser, signIn, LogOut, logInByGoogle, loading, setloading, logInByGithub }
+    return (
+        <AuthContext.Provider value={authinfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default ContextProvider
